0597521b7b4d0bf1843d6789b92df970e12e6b03
孔旭峰 <kongxufeng@htyunwang.com>
2020-09-14 14:24:24 +0800

    修复解锁2

--- src/test/java/utils/AppDemo.java
+++ src/test/java/utils/AppDemo.java
@@ -56,14 +56,16 @@ public class AppDemo {
 	@Test
 	public void plus()  throws Exception {
 		boolean flag = false;
+		int random = new Random().nextInt(10000)+5000;
+		//int random = 5000;
+		logger.info("等待时间1"+random);
+		Thread.sleep(random);
         if (!driver.currentActivity().contains("com.seeyon.cmp.ui.main.MainActivity")){
             int h = driver.manage().window().getSize().height;
             int w = driver.manage().window().getSize().width;
             driver.swipe(new Double(w*0.5).intValue(),new Double(h*0.75).intValue(),new Double(w*0.5).intValue(),new Double(h*0.25).intValue() ,1000);
         }
-		int random = new Random().nextInt(20000)+5000;
-		//int random = 5000;
-		logger.info("等待时间"+random);
+		logger.info("等待时间2"+random);
 		Thread.sleep(random);
 		Boolean a = driver.findElements(By.xpath("//android.widget.TextView[@text='跳过']")).isEmpty();
 		if (a == false){
