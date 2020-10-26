fc7e6a2d77019edff6567229509bc7b56ee82d97
kongxufeng <kongxufeng@htyunwang.com>
2020-10-26 14:17:02 +0800

    修改页面加载超时时间和元素等待超时时间

--- src/test/java/testcase/AppCaseBase.java
+++ src/test/java/testcase/AppCaseBase.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
@@ -23,14 +24,13 @@ public class AppCaseBase {
         int h = driver.manage().window().getSize().height;
         int w = driver.manage().window().getSize().width;
         logger.info("获取尺寸:"+"height="+h+",width"+w);
-        Thread.sleep(5000);
         boolean flag = BasePage.swipe_up(w,h,driver);
         logger.info("是否进行解锁："+flag);
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        //driver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
-    @AfterClass
+    @AfterSuite
     public void tearDown() {
         driver.quit();
     }
