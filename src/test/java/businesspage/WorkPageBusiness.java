ba82525191a93c6a7a69cfd0fdce5529afaa6bfb
kongxufeng <kongxufeng@htyunwang.com>
2020-11-24 09:34:01 +0800

    修改等待时间

--- src/test/java/businesspage/WorkPageBusiness.java
+++ src/test/java/businesspage/WorkPageBusiness.java
@@ -16,6 +16,7 @@ public class WorkPageBusiness {
     }
 
     public void run() throws Exception {
+        Thread.sleep(3000);
         //判断是否出现跳过、返回登录页按钮，并点击
         if(!workPageHandle.跳过按钮元素为空()){
             workPageHandle.跳过按钮_点击();
@@ -25,10 +26,13 @@ public class WorkPageBusiness {
             workPageHandle.登录按钮_点击();
         }
         //进入工作台签到页面
+        Thread.sleep(3000);
         workPageHandle.工作台按钮_点击();
        // pageHandle.等待签到按钮可用();
+        Thread.sleep(3000);
         workPageHandle.签到按钮_点击();
         //校验位置信息
+        Thread.sleep(3000);
         workPageHandle.校验位置("航天云网大厦");
         //打卡并断言
         boolean flag = workPageHandle.打卡();
